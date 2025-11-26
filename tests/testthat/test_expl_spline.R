0da61cde886e7a0c4d53544a5703c7735c5d8929
Lubomir Antoni <lubomir.antoni@upjs.sk>
2025-11-26 23:30:02 +0000

    version 1.2.10

--- tests/testthat/test_expl_spline.R
+++ tests/testthat/test_expl_spline.R
@@ -1,96 +1,146 @@
 library(testthat)
 library(ICSsmoothing)
 
-test_that("cics_unif_explicit works correctly.", {
-  n <- as.integer(runif(1, 3, 100))
-  a <- -10
-  b <- 10
-  uu <- c(seq(a, b, length.out = n + 2))
-  yy <- c(runif(n + 2, -20, 20))
-  d <- c(runif(2, -20, 20))
-  clrs <- c(sample(colours(), 2))
+n_from_uu <- function(uu) {
+  length(uu) - 2L
+}
+
+test_that("cics_unif_explicit constructs a C2 spline on a uniform grid", {
+  n <- 5L
+  a <- -2
+  b <- 3
+  
+  uu <- seq(a, b, length.out = n + 2)
+  
+  yy <- c(-1, 0.5, 2, -0.5, 1.5, -2, 0.7) 
+  stopifnot(length(yy) == n + 2)
+  
+  d  <- c(0.3, -0.8)  
+  
+  clrs <- c("black", "red")
+  
   exp_sp <- cics_unif_explicit(a, b, yy, d, clrs)
-  fl <- rep(0, n)
-  fr <- rep(0, n)
-  dl <- rep(0, n)
-  dr <- rep(0, n)
-  ddl <- rep(0, n)
-  ddr <- rep(0, n)
-  for (i in 1:(length(uu) - 2)) {
+  
+  expect_length(exp_sp$spline_polynomial, n + 1L)
+  
+  fl  <- fr  <- numeric(n)
+  dl  <- dr  <- numeric(n)
+  ddl <- ddr <- numeric(n)
+  
+  for (i in 1:(length(uu) - 2L)) {
     fv_l <- as.function(exp_sp$spline_polynomial[[i]])
-    fv_r <- as.function(exp_sp$spline_polynomial[[i + 1]])
-    fl[i] <- fv_l(uu[i + 1])
-    fr[i] <- fv_r(uu[i + 1])
+    fv_r <- as.function(exp_sp$spline_polynomial[[i + 1L]])
+    
+    fl[i] <- fv_l(uu[i + 1L])
+    fr[i] <- fv_r(uu[i + 1L])
+    
     poly_der_l <- deriv(exp_sp$spline_polynomial[[i]])
+    poly_der_r <- deriv(exp_sp$spline_polynomial[[i + 1L]])
+    
     der_l <- as.function(poly_der_l)
-    poly_der_r <- deriv(exp_sp$spline_polynomial[[i + 1]])
     der_r <- as.function(poly_der_r)
-    dl[i] <- der_l(uu[i + 1])
-    dr[i] <- der_r(uu[i + 1])
+    
+    dl[i] <- der_l(uu[i + 1L])
+    dr[i] <- der_r(uu[i + 1L])
+    
     der2l <- as.function(deriv(poly_der_l))
     der2r <- as.function(deriv(poly_der_r))
-    ddl[i] <- der2l(uu[i + 1])
-    ddr[i] <- der2r(uu[i + 1])
+    
+    ddl[i] <- der2l(uu[i + 1L])
+    ddr[i] <- der2r(uu[i + 1L])
   }
-  y_l <- as.function(exp_sp$spline_polynomial[[1]])(uu[1])
-  y_r <- as.function(exp_sp$spline_polynomial[[n + 1]])(uu[n +
-    2])
-  ext_d <- c(as.function(deriv(exp_sp$spline_polynomial[[1]]))(uu[1]),
-    as.function(deriv(exp_sp$spline_polynomial[[n + 1]]))(uu[length(uu)]))
-
-  tf1 <- isTRUE(all.equal(fl, fr,tolerance = 1e-3)) && isTRUE(all.equal(fl, yy[2:(length(yy) -
-    1)],tolerance = 1e-3))
-  tf2 <- isTRUE(all.equal(y_l, yy[1],tolerance = 1e-3)) && isTRUE(all.equal(y_r, yy[n + 2],tolerance = 1e-3))
-  tf3 <- isTRUE(all.equal(dl, dr,tolerance = 1e-3)) && isTRUE(all.equal(ddl, ddr,tolerance = 1e-3)) && isTRUE(all.equal(ext_d,
-    d,tolerance = 1e-3))
-  is_spline <- tf1 && tf2 && tf3
-
-  expect_equal(is_spline, TRUE)
+  
+  y_l <- as.function(exp_sp$spline_polynomial[[1L]])(uu[1L])
+  y_r <- as.function(exp_sp$spline_polynomial[[n + 1L]])(uu[n + 2L])
+  
+  ext_d <- c(
+    as.function(deriv(exp_sp$spline_polynomial[[1L]]))(uu[1L]),
+    as.function(deriv(exp_sp$spline_polynomial[[n + 1L]]))(uu[length(uu)])
+  )
+  
+  expect_equal(fl, fr, tolerance = 1e-3,
+               info = "Left/right function values at internal knots differ (uniform grid).")
+  expect_equal(fl, yy[2:(length(yy) - 1L)], tolerance = 1e-3,
+               info = "Function values at internal knots do not match yy (uniform grid).")
+  
+  expect_equal(y_l, yy[1L],    tolerance = 1e-3,
+               info = "Left boundary value does not match yy[1] (uniform grid).")
+  expect_equal(y_r, yy[n + 2L], tolerance = 1e-3,
+               info = "Right boundary value does not match yy[n + 2] (uniform grid).")
+  
+  expect_equal(dl,  dr,  tolerance = 1e-3,
+               info = "First derivative is not continuous at internal knots (uniform grid).")
+  expect_equal(ddl, ddr, tolerance = 1e-3,
+               info = "Second derivative is not continuous at internal knots (uniform grid).")
+  expect_equal(ext_d, d, tolerance = 1e-3,
+               info = "Boundary derivatives do not match specified d (uniform grid).")
 })
 
-test_that("cics_explicit works correctly.", {
-  n <- as.integer(runif(1, 3, 100))
-  a <- -10
-  b <- 10
-  uu <- c(sort(runif(n + 2, a, b)))
-  yy <- c(runif(n + 2, -20, 20))
-  d <- c(runif(2, -20, 20))
-  clrs <- c(sample(colours(), 2))
+
+test_that("cics_explicit constructs a C2 spline on a non-uniform grid", {
+  uu <- c(-2.0, -1.3, -0.4, 0.1, 1.0, 2.2, 3.0)
+  n  <- n_from_uu(uu)  # očakávame n = 5L
+  
+  yy <- c(-0.7, 1.2, -1.0, 0.3, 2.1, -0.4, 0.8)
+  stopifnot(length(yy) == length(uu))
+  
+  d  <- c(-0.2, 0.5)
+  
+  clrs <- c("blue", "green")
+  
   exp_sp <- cics_explicit(uu, yy, d, clrs)
-  fl <- rep(0, n)
-  fr <- rep(0, n)
-  dl <- rep(0, n)
-  dr <- rep(0, n)
-  ddl <- rep(0, n)
-  ddr <- rep(0, n)
-  for (i in 1:(length(uu) - 2)) {
+  
+  expect_length(exp_sp$spline_polynomial, n + 1L)
+  
+  fl  <- fr  <- numeric(n)
+  dl  <- dr  <- numeric(n)
+  ddl <- ddr <- numeric(n)
+  
+  for (i in 1:(length(uu) - 2L)) {
     fv_l <- as.function(exp_sp$spline_polynomial[[i]])
-    fv_r <- as.function(exp_sp$spline_polynomial[[i + 1]])
-    fl[i] <- fv_l(uu[i + 1])
-    fr[i] <- fv_r(uu[i + 1])
+    fv_r <- as.function(exp_sp$spline_polynomial[[i + 1L]])
+    
+    fl[i] <- fv_l(uu[i + 1L])
+    fr[i] <- fv_r(uu[i + 1L])
+    
     poly_der_l <- deriv(exp_sp$spline_polynomial[[i]])
+    poly_der_r <- deriv(exp_sp$spline_polynomial[[i + 1L]])
+    
     der_l <- as.function(poly_der_l)
-    poly_der_r <- deriv(exp_sp$spline_polynomial[[i + 1]])
     der_r <- as.function(poly_der_r)
-    dl[i] <- der_l(uu[i + 1])
-    dr[i] <- der_r(uu[i + 1])
+    
+    dl[i] <- der_l(uu[i + 1L])
+    dr[i] <- der_r(uu[i + 1L])
+    
     der2l <- as.function(deriv(poly_der_l))
     der2r <- as.function(deriv(poly_der_r))
-    ddl[i] <- der2l(uu[i + 1])
-    ddr[i] <- der2r(uu[i + 1])
+    
+    ddl[i] <- der2l(uu[i + 1L])
+    ddr[i] <- der2r(uu[i + 1L])
   }
-  y_l <- as.function(exp_sp$spline_polynomial[[1]])(uu[1])
-  y_r <- as.function(exp_sp$spline_polynomial[[n + 1]])(uu[n +
-    2])
-  ext_d <- c(as.function(deriv(exp_sp$spline_polynomial[[1]]))(uu[1]),
-    as.function(deriv(exp_sp$spline_polynomial[[n + 1]]))(uu[length(uu)]))
-
-  tf1 <- isTRUE(all.equal(fl, fr,tolerance = 1e-3)) && isTRUE(all.equal(fl, yy[2:(length(yy) -
-    1)],tolerance = 1e-3))
-  tf2 <- isTRUE(all.equal(y_l, yy[1],tolerance = 1e-3)) && isTRUE(all.equal(y_r, yy[n + 2],tolerance = 1e-3))
-  tf3 <- isTRUE(all.equal(dl, dr,tolerance = 1e-3)) && isTRUE(all.equal(ddl, ddr,tolerance = 1e-3)) && isTRUE(all.equal(ext_d,
-    d,tolerance = 1e-3))
-  is_spline <- tf1 && tf2 && tf3
-
-  expect_equal(is_spline, TRUE)
+  
+  y_l <- as.function(exp_sp$spline_polynomial[[1L]])(uu[1L])
+  y_r <- as.function(exp_sp$spline_polynomial[[n + 1L]])(uu[n + 2L])
+  
+  ext_d <- c(
+    as.function(deriv(exp_sp$spline_polynomial[[1L]]))(uu[1L]),
+    as.function(deriv(exp_sp$spline_polynomial[[n + 1L]]))(uu[length(uu)])
+  )
+  
+  expect_equal(fl, fr, tolerance = 1e-3,
+               info = "Left/right function values at internal knots differ (non-uniform grid).")
+  expect_equal(fl, yy[2:(length(yy) - 1L)], tolerance = 1e-3,
+               info = "Function values at internal knots do not match yy (non-uniform grid).")
+  
+  expect_equal(y_l, yy[1L],     tolerance = 1e-3,
+               info = "Left boundary value does not match yy[1] (non-uniform grid).")
+  expect_equal(y_r, yy[n + 2L], tolerance = 1e-3,
+               info = "Right boundary value does not match yy[n + 2] (non-uniform grid).")
+  
+  expect_equal(dl,  dr,  tolerance = 1e-3,
+               info = "First derivative is not continuous at internal knots (non-uniform grid).")
+  expect_equal(ddl, ddr, tolerance = 1e-3,
+               info = "Second derivative is not continuous at internal knots (non-uniform grid).")
+  expect_equal(ext_d, d, tolerance = 1e-3,
+               info = "Boundary derivatives do not match specified d (non-uniform grid).")
 })
